e9f5caf9a64c8c03348cbb1a00eccd5709a72b2e
suadnesimi1 <suadnesimi12@gmail.com>
2021-04-09 11:56:55 +0200

    initial comment

--- src/main/java/Repository/ExamRepo.java
+++ src/main/java/Repository/ExamRepo.java
@@ -1,4 +1,36 @@
 package Repository;
 
+import Connection.DbConnection;
+import Domain.Exam;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamRepo {
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public List<Exam> getExam() {
+        List<Exam> examsList = new ArrayList<>();
+
+        String query = "select * from exams";
+        try {
+            ps = DbConnection.getConnection().prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Exam exam = new Exam();
+                exam.setId(rs.getString(1));
+                examsList.add(exam);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbConnection.closeAll(ps);
+
+        }
+        return examsList;
+    }
 }
---
+++ src/main/java/Resource/ExamResource.java
@@ -0,0 +1,23 @@
+package Resource;
+
+import Domain.Exam;
+import Repository.ExamRepo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+public class ExamResource {
+
+@Path("/exam")
+    public class examResource{
+    ExamRepo repo = new ExamRepo();
+
+    @GET
+    @Consumes({MediaType.APPLICATION_JSON})
+    public List<Exam> getExam(){
+        return repo.getExam();
+    }
+}
+}
